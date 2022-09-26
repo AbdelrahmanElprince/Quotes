5ec7bd2ed4e847b264db1f6997240e93772dd3de
AbdelrahmanElprince <abdelrahman_elprince@yahoo.com>
2022-09-26 15:31:03 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+let quote = [
+    {'author': 'Jim Rohn', 
+     'quote': 'Beware of what you become in pursuit of what you want.'
+    },
+    {'author': 'Epictetus', 
+     'quote': 'It\'s not what happens to you, but how you react to it that matters.'
+    },
+    {'author': 'Frank Sinatra', 
+     'quote': 'The best revenge is massive success.'
+    },
+    {'author': 'Wayne Gretzy', 
+     'quote': 'You miss 100% of the shots you don\'t take.'
+    },
+    {'author': 'Nelson Mandela', 
+     'quote': 'Resentment is like drinking poison and waiting for your enemies to die.'
+    },
+    {'author': 'Elbert Hubbard', 
+     'quote': 'Do not take life too seriously. You will not get out alive.'
+    }
+
+];
+function getQoutes(){
+    var num = Math.floor(Math.random()*quote.length);
+    console.log(num);
+
+    document.getElementById('quote').innerHTML = quote[num].quote;
+    document.getElementById('auther').innerHTML = quote[num].author;
+
+}
